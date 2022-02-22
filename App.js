b44fe1cf07ad90f870a9d76e27428bf336952040
Horizones.studio <77297401+Horizones-Studio@users.noreply.github.com>
2022-02-22 14:20:39 +0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {config, animated, useTransition} from "@react-spring/native";
+import {useState} from "react";
 
 export default function App() {
+
+  // const transitions = useTransition(true, {
+  //   from: { opacity: 0 },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 },
+  //   delay: 200,
+  //   config: config.molasses,
+  // })
+  // return transitions(
+  //
+  //    (styles, item) => item && <animated.View style={styles}>
+  //      <View style={{
+  //        flex: 1,
+  //        justifyContent: "center",
+  //        alignItems: "center"
+  //      }}>
+  //        <Text style={{
+  //          fontSize: 32
+  //        }}>110819026</Text>
+  //        <Text style={{
+  //          fontSize: 32,
+  //          fontWeight: "bold"
+  //        }}>陳亮禎</Text>
+  //      </View>
+  //    </animated.View>
+  // )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+
+
+
+      <Text style={{
+        fontSize: 32
+      }}>110819026</Text>
+      <Text style={{
+        fontSize: 32,
+        fontWeight: "bold"
+      }}>陳亮禎</Text>
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
